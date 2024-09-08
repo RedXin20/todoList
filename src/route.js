2c0b6ed3aab661572a081e164211d08bb718981c
Yosef <Yosef234@outlook.dk>
2024-09-08 22:06:50 +0200

    bug fix

--- src/route.js
+++ src/route.js
@@ -3,7 +3,9 @@ let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
 addToDoButton.addEventListener("click", function () {
-  var paragraph = document.createElement("p"); // Create a new paragraph element
+  if (!inputField.value.trim()) return; // Prevent adding empty tasks
+
+  const paragraph = document.createElement("p"); // Create a new paragraph element
   paragraph.innerText = inputField.value; // Set its content to the input field's value
   toDoContainer.appendChild(paragraph); // Attach paragraph to the todo container
   inputField.value = ""; // Clearing the input field once prompt has been added.
