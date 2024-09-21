b41229f8eea836b6f7e672cd45400493ae78165f
Yosef <Yosef234@outlook.dk>
2024-09-22 01:23:54 +0200

    overhaul + implementation

--- src/route.js
+++ src/route.js
@@ -1,21 +1,29 @@
 const addToDoButton = document.getElementById("addToDo");
 const toDoContainer = document.getElementById("toDoContainer");
 const inputField = document.getElementById("inputField");
+const toggleButton = document.getElementById("theme-toggle");
 
-addToDoButton.addEventListener("click", function () {
+function addToDo() {
   if (!inputField.value.trim()) return; // Prevent adding empty tasks
 
   const paragraph = document.createElement("p"); // Create a new paragraph element
   paragraph.innerText = inputField.value; // Set its content to the input field's value
+  paragraph.classList.add("text-black", "dark:text-white");
   toDoContainer.appendChild(paragraph); // Attach paragraph to the todo container
-  inputField.value = ""; // Clearing the input field once prompt has been added.
+  inputField.value = ""; // Clearing the input field once prompt has been added.}
 
   paragraph.addEventListener("click", function () {
     paragraph.remove("p");
   });
-});
+}
 
-const toggleButton = document.getElementById("theme-toggle");
+addToDoButton.addEventListener("click", addToDo);
+
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addToDo();
+  }
+});
 
 toggleButton.addEventListener("click", function () {
   document.body.classList.toggle("dark");
